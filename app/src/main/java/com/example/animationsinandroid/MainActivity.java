d18c0fa8564367f3ef1792ac7113d38231bcc548
Sid <siddharthbisht99@gmail.com>
2020-06-09 19:18:50 +0530

    Basic logic for Dnd TicTacToe

--- app/src/main/java/com/example/animationsinandroid/MainActivity.java
+++ app/src/main/java/com/example/animationsinandroid/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.animationsinandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    int turn = 1;
+    int[] status = new int[9];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,4 +19,21 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    public void tapped(View view) {
+        ImageView v = (ImageView) view;
+        if (turn == 1 && status[Integer.parseInt(v.getTag().toString())-1] == 0) {
+            v.setImageResource(R.drawable.brain);
+            int a = Integer.parseInt(v.getTag().toString());
+            status[a-1] = 1;
+            turn = 2;
+        } else if (turn == 2 && status[Integer.parseInt(v.getTag().toString())-1] == 0){
+            v.setImageResource(R.drawable.heart);
+            int a = Integer.parseInt(v.getTag().toString());
+            status[a-1] = 2;
+            turn = 1;
+        }
+
+    }
+
 }
